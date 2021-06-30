afee143a132cc6c5bc340972876b336c856e4027
Jakub <kontakt@jakubpiskorz.pl>
2021-06-30 13:01:01 +0200

    changed redirection

--- src/pages/edytuj.js
+++ src/pages/edytuj.js
@@ -1,10 +1,19 @@
-import React, {useState, useEffect} from "react"
-import {Router, Route, Redirect} from "@reach/router"
+import React, { useState, useEffect } from "react"
+import { Router, Route, Redirect } from "@reach/router"
 
 const Edytuj = () => {
-    const loadWordpressAdmin = () => window.location.href = "http://yogakamala.jakubpiskorz.pl/wp-admin";
-    
-    return <button className="yoga-button" style={{marginLeft: "40px"}} onClick={loadWordpressAdmin}>Click to login</button>
+  const loadWordpressAdmin = () =>
+    (window.location.href = "https://yoga.jakubpiskorz.dev/wp-admin")
+
+  return (
+    <button
+      className="yoga-button"
+      style={{ marginLeft: "40px" }}
+      onClick={loadWordpressAdmin}
+    >
+      Click to login
+    </button>
+  )
 }
 
 export default Edytuj
